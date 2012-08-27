6ab93068fe647575671452df8bd04a8e638d892e
U-Angler\Ian <Ian@Angler.(none)>
2012-08-27 07:35:26 -0700

    solar time calculations

--- src/main.c
+++ src/main.c
@@ -2,39 +2,75 @@
 #include <stdio.h>
 #include <math.h>
 
+
+
 // the typedef should go in the header
 typedef struct {
 	int x; 
 	int y;
 } point;
 
-int linear_eq(double m, int x, double b){
-	int y = m * x + b;
+int linear_eq(int y2, int x1, int x2){
+	int m = round(y2 / x1);
+	int y = m * (round(x2 - x1)) + y2;
 	return y;
 }
 
-point* make_line(point a, point b){
-	/*
-	find the equation for line
-	allocate array big enough to fit the distance between them
-	for(int i = 0; i < |b.x - a.x|; i++){ 
-		
-	}
-		
+void draw_line(int x0, int y0, int x1, int y1, char render_char){
+	int dx = abs(x1-x0);
+	int dy = abs(y1-y0);
+	int sx = x0 < x1 ? 1 : -1;
+	int sy = y0 < y1 ? 1 : -1;
 
-	abs(b.y - a.y) = a.y/b.x * abs(a.x - b.x)
-	*/
+	// this examines how steep the slope is
+	int err = dx - dy;
+
+	for(;;){
+		mvaddch(y0, x0, render_char);
+		if(x0 == x1 && y0 == y1) break;
+		int e2 = 2*err; 
+
+		// i think this examines whether or not the slope is steep enough to have Y be the driving force. A steep line should iterate over Y instead of X.
+		if(e2 > -dy){
+			err -= dy; // reset the accumulation to 0
+			x0 += sx;
+		} else if(e2 < dx){
+			err += dx; // accumulate more fractions
+			y0 += sy;
+		}
+	}
 }
 
-point* draw_line(point* line){
+double get_lstm(double hour_diff){
+	return 
+}
 
+double get_hour_angle(double hour_diff, int days, double lng){
+	double lstm = 15 * hour_diff;
+	int b = 360/365 * (days-81);
+	double time = 9.87 * sin(2*b) - 7.53 * cos(b) - 1.5*sin(b);
+	double tc = 4*(lstm - lng) + time;
+	double lst = hour_diff + tc / 60;
+	
+	return (double) 15 * (lst-12);
+}
+
+double get_time = function(){
+	return 9.87
 }
 
 int main(){
 	
 	point a;
-	a.x = 1;
-	a.y = 1;
+	a.x = 5;
+	a.y = 5;
+
+	point b;
+	b.x = 0;
+	b.y = 0;
+
+	
+	// printf("%d", y);
 
 	// Initialize ncurses
 	initscr();
@@ -45,20 +81,17 @@ int main(){
 	curs_set(0);	
 
 	// test symbol
-	int row = 10, col = 10;
 	char main_char = '@';
 	char ch;
 
-
 	// Rendering loop
+	draw_line(1,1,20,10, '@');
+	refresh();
 	while(1) {
 		ch = getch();
 		if(ch == 'q' || ch == 'Q') {
 			break;
-			clear();
-		} else {
-			mvaddch(row, col, main_char);
-			refresh();
 		}
 	}
+	endwin(); // clear ncurses's junk
 }
