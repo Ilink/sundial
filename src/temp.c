ea4eacfe609f906ccdebb72cbe142596bf3049b8
ilink <ian@linkportfolio.net>
2012-08-29 15:29:40 -0700

    removed stuff

--- src/temp.c
+++ src/temp.c
@@ -13,28 +13,16 @@ while(g < 360){
 double lambda = L + 1.915 * sin(g) + 0.020 * sin(2*g);
 double epsilon = 23.439 - 0.0000004 * d;
 
-// RA and Declination
+// RA (alpha) and Declination (delta)
 
-	double y = cos(epsilon) * sin(lambda);
-	double x = cos(lambda);
+double y = cos(epsilon) * sin(lambda);
+double x = cos(lambda);
 
-	double a = arctan(y/x);
-	double alpha;
-	if(x < 0) alpha = a + 180;
-	if(y < 0 && x > 0) alpha = a + 360;
-	else alpha = a;
+double a = arctan(y/x);
+double alpha;
+if(x < 0) alpha = a + 180;
+if(y < 0 && x > 0) alpha = a + 360;
+else alpha = a;
 
-	double delta = arcsin(sin(epsilon)*sin(lambda));
+double delta = arcsin(sin(epsilon)*sin(lambda));
 
-   // If X < 0 then alpha = a + 180
-   // If Y < 0 and X > 0 then alpha = a + 360
-   // else alpha = a
-
-   // Y =  0.6489924
-   // X = -0.7068507
-
-   // a = -42.556485
-   // alpha = -42.556485 + 180 = 137.44352 (degrees)
-   
-   // delta = arcsin(sin(epsilon)*sin(lambda))
-   //       = 16.342193 degrees
