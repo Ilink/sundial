158fd15c5210787c91b9f190df270ddf47baf6a5
ilink <ian@linkportfolio.net>
2012-08-31 11:52:50 -0700

    somewhat close to the right calculation

--- src/main.c
+++ src/main.c
@@ -39,6 +39,7 @@ void draw_line(int x0, int y0, int x1, int y1, char render_char){
 	}
 }
 
+// returns hour + fraction
 double get_ut(){
 	time_t rawtime;
 	struct tm *ptm;
@@ -46,11 +47,14 @@ double get_ut(){
 	time(&rawtime);
 	ptm = gmtime(&rawtime);
 
-	double sec = ptm->tm_sec / 60.0 / 60.0;
+	double sec = ptm->tm_sec / 3600.0;
 	double min = ptm->tm_min / 60.0;
-	return ptm->tm_hour + sec + min;
+	double time = ptm->tm_hour + sec + min;
+
+	return time;
 }
 
+// uses UT, not local time
 double get_jd(int year, int month, int day){
 	double UT;
 	UT = get_ut();
@@ -58,11 +62,9 @@ double get_jd(int year, int month, int day){
 	int a = (month+9)/12;
 	int b = (7*(year+a))/4;
 	int c = 275 * month / 9;
-	return 367*year - b + c + day + 1721013.5 + UT/24;
-
-
-	// return (double) day-32075+1461*(year+4800+(month-14)/12) /
-	// 	4+367*(month-2-(month-14)/12*12)/12-3*((year+4900+(month-14)/12)/100)/4;
+	int delta = year - 1949;
+	return 32916.5 + delta * 365 + day + UT / 24;
+	// return 367*year - b + c + day + 1721013.5 + UT/24;
 }
 
 // ecliptic_coord sun_pos(double jd){
@@ -87,186 +89,156 @@ double get_jd(int year, int month, int day){
 // 	coord.lng = 1.0;
 // }
 
-point_3d celestial(double jd){
+s_coord2 celestial(double jd, double lat, double lng){
+	double hour = get_ut();
+	printf("Hour: %f\n", hour);
 	double rads = 180/PI;
 
 	double j2k = get_jd(2000, 1, 1);
-	jd = j2k-jd;
+	double time;
+	time = jd-j2k;
+	time = jd-51545;
 
-	double L = 280.461 * rads + .9856474 * rads * jd;
-	while(L < 360){
-		L += 360;
+	printf("Time: %f\n", time);
+
+	double L = 280.460 + .9856474 * time;
+	while(L > 360){
+		L -= 360;
 	}
 
-	double g = 357.528 * rads + .9856003 * rads * jd;
-	while(g < 360){
-		g += 360;
+	printf("L: %f\n", L);
+
+	double g = 357.528 + .9856003 * time;
+	while(g > 360){
+		g -= 360;
 	}
+
+	printf("G: %f\n", g);
 
 	double lambda = L + 1.915 * sin(g) + 0.020 * sin(2*g);
-	double epsilon = 23.439 - 0.0000004 * jd;
+	while(lambda > 360){
+		lambda -= 360;
+	}
+
+	printf("Lambda: %f\n", L);
+
+	double epsilon = 23.439 - 0.0000004 * time;
+	printf("Epsilon: %f\n", epsilon);
+	lambda *= rads;
+	epsilon *= rads;
 
 	// RA (alpha) and Declination (delta)
 	double y = cos(epsilon) * sin(lambda);
 	double x = cos(lambda);
 
-	double a = atan2(y,x);
+	printf("X: %f\n", x);
+	printf("Y: %f\n", y);
+
+	double a = atan(y/x);
+	printf("A: %f\n", a);
 	double alpha;
-	if(x < 0) alpha = a + 180;
-	if(y < 0 && x > 0) alpha = a + 360;
+	if(x < 0) alpha = a + PI;
+	if(y < 0 && x > 0) alpha = a + 2*PI;
 	else alpha = a;
-
+	printf("Alpha: %f\n", alpha);
 	double delta = asin(sin(epsilon)*sin(lambda));
-	double R = 1.00014 - 0.01671*cos(g) - 0.00014*cos(2*g);
 
-	double X = R*cos(lambda);
-	double Y = R*cos(epsilon)*sin(lambda);
-	double Z = R*sin(epsilon)*sin(lambda);
+	printf("Delta: %f\n", delta);
 
-	celestial_coord coords;
-	coords.delta = delta;
-	coords.alpha = alpha;
-	coords.r = 1.00014 - 0.01671*cos(g) - 0.00014*cos(2*g);
+	// Local coordinates
+	double gmst = 6.697375 + .0657098242 * time + hour;
+	while(gmst < 24){
+		gmst += 24;
+	}
+	printf("GMST: %f\n", gmst);
 
-	point_3d point;
-	point.x = X;
-	point.y = Y;
-	point.z = Z;
+	double lmst = gmst + lng / 15;
+	while(lmst > 24){
+		lmst -= 24;
+	}
+	lmst *=  rads;
+	printf("LMST: %f\n", lmst);
+
+	double ha = lmst - alpha;
+	if(ha < -1*PI) ha += (2*PI);
+	if(ha > PI) ha -= (2*PI);
+
+	lat *= rads;
+
+	// azi and elevation
+	double el = asin(sin(delta)*sin(lat)+cos(delta)*cos(lat)*cos(ha));
+	double az = asin(-1*cos(delta)*sin(ha)/cos(el));
+
+	double cos_az_pos;
+	if(0 < sin(delta) - sin(el)*sin(lat)){
+		if(sin(az) < 0) az += (2*PI);
+	} else {
+		az -= PI;
+	}
+
+	// el /= rads;
+	// az /= rads;
+	// lat /= rads;
+
+	s_coord2 coord;
+	coord.azimuth = az;
+	coord.latitude = lat;
+	coord.elevation = el;
+
+	return coord;
+
+	// double R = 1.00014 - 0.01671*cos(g) - 0.00014*cos(2*g);
+
+	// double X = R*cos(lambda);
+	// double Y = R*cos(epsilon)*sin(lambda);
+	// double Z = R*sin(epsilon)*sin(lambda);
+
+	// celestial_coord coords;
+	// coords.delta = delta;
+	// coords.alpha = alpha;
+	// coords.r = 1.00014 - 0.01671*cos(g) - 0.00014*cos(2*g);
+
+	// point_3d point;
+	// point.x = X;
+	// point.y = Y;
+	// point.z = Z;
 	// double X = R*cos(lambda);
 	// double Y = R*cos(epsilon)*sin(lambda);
 	// double Z = R*sin(epsilon)*sin(lambda);
 	
-	return point;
+	// return point;
 }
 
-s_coord spherical(int day, int hour, int minute, int second, double timezone){
+s_coord spherical(int day, int hour, int minute, int sec, double timezone, double lat, double lng){
 
-	double frac_year = 2*PI/365*(day-1+(hour-12)/24);
-	double eqtime = 229.18*(0.000075+0.001868*cos(frac_year)-0.032077*sin(frac_year)-0.014615*cos(2*frac_year)-0.040849*sin(2*frac_year));
+	double frac_year = (2*PI/365)*(day-1+((hour-12)/24));
+
+	printf("%f\n", frac_year);
+
+	double eqtime = 229.18*(0.000075+0.001868*cos(frac_year)-1.0*0.032077*sin(frac_year)-0.014615*cos(2.0*frac_year)-0.040849*sin(2.0*frac_year));
+
+	printf("%f\n", eqtime);
+	eqtime = -0.21;
 
 	double decl = 0.006918 - 0.399912*cos(frac_year)+0.070257*sin(frac_year) - 0.006758*cos(2*frac_year) + 0.000907*sin(2*frac_year)-0.002697*cos(3*frac_year)+0.00148*sin(3*frac_year);
+	printf("%f\n", decl);
+
 
 	double time_offset = eqtime-4*lng + 60*timezone;
 	double tst = hour*60+minute+sec/60+time_offset;
 	double ha = (tst/4) - 180;
 
+	double zenith = acos(sin(lat)*sin(decl)+cos(lat)*cos(decl)*cos(ha));
+	double azimuth = -1*(sin(lat)*cos(zenith)-sin(decl)) / cos(lat)*sin(zenith);
+	
+
 	s_coord coord;
-	s_coord.zenith = 1.0;
-	s_coord.altitude = 1.0;
-	s_coord.r = 1.0;
+	coord.zenith = zenith;
+	coord.altitude = azimuth;
+	coord.r = 1.0;
+	return coord;
 }
 
-
-double awesome_calc(int year, int month, int day, 
-	int _hour, int min, int sec, double lat, double lng){
-
-	double twopi = 2*PI;
-	double rad = PI/180;
-
-	int days_month[] = {0,31,28,31,30,31,30,31,31,30,31,30};
-	int days = 0;
-	// this might be off by a month..use month-1?
-	int i;
-	for(i=0; i < month; i++){
-		days += days_month[i];
-	}
-	// does not handle leap years like a boss
-	days += day;
-
-	double hour = _hour + min/60 + sec/3600;
-	int delta = year - 1949;
-	double jd = 32916.5 + delta * 365 + day + hour / 24;
-	double time = jd - 51545;
-
-	return (double) 1.0;
-
-
-
-// 	sunPosition <- function(year, month, day, hour=12, min=0, sec=0,
-//                         lat=46.5, long=6.5) {
-//   twopi <- 2 * pi
-//   deg2rad <- pi / 180
-
-//   # Get day of the year, e.g. Feb 1 = 32, Mar 1 = 61 on leap years
-//   month.days <- c(0,31,28,31,30,31,30,31,31,30,31,30)
-//   day <- day + cumsum(month.days)[month]
-//   leapdays <- year %% 4 == 0 & (year %% 400 == 0 | year %% 100 != 0) & day >= 60
-//   day[leapdays] <- day[leapdays] + 1
-
-//   # Get Julian date - 2400000
-//   hour <- hour + min / 60 + sec / 3600 # hour plus fraction
-//   delta <- year - 1949
-//   leap <- trunc(delta / 4) # former leapyears
-//   jd <- 32916.5 + delta * 365 + leap + day + hour / 24
-
-//   # The input to the Atronomer's almanach is the difference between
-//   # the Julian date and JD 2451545.0 (noon, 1 January 2000)
-//   time <- jd - 51545.
-
-//   # Ecliptic coordinates
-
-//   # Mean longitude
-//   mnlong <- 280.460 + .9856474 * time
-//   mnlong <- mnlong %% 360
-//   mnlong[mnlong < 0] <- mnlong[mnlong < 0] + 360
-
-//   # Mean anomaly
-//   mnanom <- 357.528 + .9856003 * time
-//   mnanom <- mnanom %% 360
-//   mnanom[mnanom < 0] <- mnanom[mnanom < 0] + 360
-//   mnanom <- mnanom * deg2rad
-
-//   # Ecliptic longitude and obliquity of ecliptic
-//   eclong <- mnlong + 1.915 * sin(mnanom) + 0.020 * sin(2 * mnanom)
-//   eclong <- eclong %% 360
-//   eclong[eclong < 0] <- eclong[eclong < 0] + 360
-//   oblqec <- 23.429 - 0.0000004 * time
-//   eclong <- eclong * deg2rad
-//   oblqec <- oblqec * deg2rad
-
-//   # Celestial coordinates
-//   # Right ascension and declination
-//   num <- cos(oblqec) * sin(eclong)
-//   den <- cos(eclong)
-//   ra <- atan(num / den)
-//   ra[den < 0] <- ra[den < 0] + pi
-//   ra[den >= 0 & num < 0] <- ra[den >= 0 & num < 0] + twopi
-//   dec <- asin(sin(oblqec) * sin(eclong))
-
-//   # Local coordinates
-//   # Greenwich mean sidereal time
-//   gmst <- 6.697375 + .0657098242 * time + hour
-//   gmst <- gmst %% 24
-//   gmst[gmst < 0] <- gmst[gmst < 0] + 24.
-
-//   # Local mean sidereal time
-//   lmst <- gmst + long / 15.
-//   lmst <- lmst %% 24.
-//   lmst[lmst < 0] <- lmst[lmst < 0] + 24.
-//   lmst <- lmst * 15. * deg2rad
-
-//   # Hour angle
-//   ha <- lmst - ra
-//   ha[ha < -pi] <- ha[ha < -pi] + twopi
-//   ha[ha > pi] <- ha[ha > pi] - twopi
-
-//   # Latitude to radians
-//   lat <- lat * deg2rad
-
-//   # Azimuth and elevation
-//   el <- asin(sin(dec) * sin(lat) + cos(dec) * cos(lat) * cos(ha))
-//   az <- asin(-cos(dec) * sin(ha) / cos(el))
-//   elc <- asin(sin(dec) / sin(lat))
-//   az[el >= elc] <- pi - az[el >= elc]
-//   az[el <= elc & ha > 0] <- az[el <= elc & ha > 0] + twopi
-
-//   el <- el / deg2rad
-//   az <- az / deg2rad
-//   lat <- lat / deg2rad
-
-//   return(list(elevation=el, azimuth=az))
-// }
-}
 
 int main(){
 
@@ -278,18 +250,25 @@ int main(){
 	b.x = 0;
 	b.y = 0;
 
+	double degs = 180.0 / PI;
+
 	double JD2 = get_jd(2012, 8, 31);
 	double J2k = get_jd(2000, 1, 1);
-	point_3d sun_pos = celestial(JD2);
+	s_coord2 sun_pos = celestial(JD2, 37.9232, 122.2937);
 
-	printf("%f\n", J2k);
+	// printf("%f\n", J2k);
 	printf("%f\n", JD2);
-	printf("%f\n", JD2-J2k);
-	printf("%f\n", get_ut());
+	// printf("%f\n", JD2-J2k);
+	// printf("%f\n", get_ut());
 
-	printf("%f\n", sun_pos.x);
-	printf("%f\n", sun_pos.y);
-	printf("%f\n", sun_pos.z);
+	// printf("%f\n", sun_pos.x);
+	// printf("%f\n", sun_pos.y);
+	// printf("%f\n", sun_pos.z);
+
+	printf("Latitude: %f\n", sun_pos.latitude * degs);
+	printf("Azimuth: %f\n", sun_pos.azimuth*degs);
+	printf("Elevation: %f\n", sun_pos.elevation*degs);
+
 	// celestial_coord test = celestial()
 
 	// // Initialize ncurses
